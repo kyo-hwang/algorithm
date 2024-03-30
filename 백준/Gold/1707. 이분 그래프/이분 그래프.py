62ff22d610d0207a7c172ee85de729e5e2a7b6c1
KyoJuneHwang <163268142+kyo-hwang@users.noreply.github.com>
2024-03-30 11:24:26 +0900

    [Gold IV] Title: 이분 그래프, Time: 1664 ms, Memory: 54504 KB -BaekjoonHub

---
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -0,0 +1,46 @@
+import sys
+sys.setrecursionlimit(10**6)
+# sys.stdin = open("input.txt","r")
+
+tN = int(sys.stdin.readline())
+
+def dfs(v,parent,depth):
+    global graph
+    global visited
+    global where
+    global answer
+
+    if visited[v]:
+        return
+    visited[v] = True
+    
+    where[v] = depth%2
+
+    for w in graph[v]:
+        if not visited[w]:
+            dfs(w,v,depth+1)
+        elif visited[w]:
+            if w!=parent:
+                #같은 홀수 또는 짝수인지
+                if where[v] == where[w]:
+                    answer = "NO"
+
+for _ in range(tN):
+    vN,eN = (int(c) for c in sys.stdin.readline().split())
+    graph = [[] for _ in range(vN)]
+    for _ in range(eN):
+        row,col = (int(c) for c in sys.stdin.readline().split())
+        graph[row-1].append(col-1)
+        graph[col-1].append(row-1)
+
+    where = [-1]*vN
+
+    visited = [False]*vN
+    
+    answer = "YES"
+    for v in range(vN):
+        if not visited[v]:
+            dfs(v,-1,0)
+        if answer =="NO":
+            break
+    print(answer)
