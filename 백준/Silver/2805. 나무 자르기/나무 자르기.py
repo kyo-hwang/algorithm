862fb5706e3d5a6648b2e23c8f5d79c120f6238d
KyoJuneHwang <163268142+kyo-hwang@users.noreply.github.com>
2024-03-31 20:58:37 +0900

    [Silver II] Title: 나무 자르기, Time: 408 ms, Memory: 204332 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -7,7 +7,7 @@ treeN,wAmount = map(int,sys.stdin.readline().rstrip().split())
 heights = [int(height) for height in sys.stdin.readline().rstrip().split(" ")]
 
 
-#나무의 길이를 잘라서 가져갈 수 있는 양을 구한다.
+#나무의 길이를 잘라서 가져갈 수 있는 양을 구함
 def calAmount(treeHeights,cutHeight):
     amount =0
     for treeHeight in treeHeights:
@@ -15,40 +15,15 @@ def calAmount(treeHeights,cutHeight):
             amount += (treeHeight-cutHeight)
     return amount
 
-heights.sort()
-fromSum = [heights[0]]
-for i in range(1,len(heights)):
-    fromSum.append(heights[i]+fromSum[i-1])
-
-def calAmountRapidly(treeHeights,fromSum,cutHeight):
-    left = 0
-    right = len(treeHeights)-1
-    
-    #톱의 높이보다 높은 나무 중 최소 값은 left
-    while left<=right:
-        mid = (left+right)//2
-        if cutHeight == treeHeights[mid]:
-            left = mid
-            break
-        elif cutHeight > treeHeights[mid]:
-            left = mid+1
-        elif cutHeight < treeHeights[mid]:
-            right = mid-1
-
-    if left==0:
-        result = fromSum[-1] - (len(treeHeights)-left)*cutHeight
-    else:
-        result = fromSum[-1] - fromSum[left-1] -(len(treeHeights)-left)*cutHeight
-    return result
 
 def binarySearch(wAmount,heights):
     bottom = 0
-    ceiling = heights[-1]
+    ceiling = max(heights)
 
     while ceiling >= bottom:
         cutHeight = (bottom + ceiling)//2
-        amount = calAmountRapidly(heights,fromSum,cutHeight)
-        # print(amount)
+
+        amount = calAmount(heights,cutHeight)
 
         if amount == wAmount:
             return cutHeight
@@ -58,5 +33,5 @@ def binarySearch(wAmount,heights):
             bottom = cutHeight+1
 
     return ceiling
-# print(fromSum)
+
 print(binarySearch(wAmount,heights))
