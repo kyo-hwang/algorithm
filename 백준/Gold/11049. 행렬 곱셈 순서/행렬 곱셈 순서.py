13d5ec525a8c550b8046c045a81b00795b628617
KyoJuneHwang <163268142+kyo-hwang@users.noreply.github.com>
2024-04-08 15:41:11 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 460 ms, Memory: 112540 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
@@ -8,8 +8,9 @@ dp = [[0]*n for _ in range(n)]
 for i in range(1,n):
     for j in range(0,n-i):
         curMin = sys.maxsize
+        mul = m[j+i][1]*m[j][0]
         for k in range(j,j+i):
-            val = dp[j][k]+dp[k+1][j+i]+m[j][0]*m[k][1]*m[j+i][1]
+            val = dp[j][k]+dp[k+1][j+i]+m[k][1]*mul
             if val < curMin:
                 curMin = val
         dp[j][j+i] = curMin
