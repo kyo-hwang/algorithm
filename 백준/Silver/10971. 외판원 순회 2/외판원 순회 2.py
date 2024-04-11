465dac9758e7238c94356d5131982d6358dff386
KyoJuneHwang <163268142+kyo-hwang@users.noreply.github.com>
2024-04-11 22:57:48 +0900

    [Silver II] Title: 외판원 순회 2, Time: 228 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,33 +1,56 @@
 import sys
-import copy
+# sys.stdin = open("input.txt","r")
+import time
+start = time.time()
 
-n = int(sys.stdin.readline().rstrip())
+n = int(sys.stdin.readline())
+graph = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
 
-routes = [[int(j) for j in sys.stdin.readline().rstrip().split(" ")] for _ in range(n)]
-visited = [False]*n
-
-global shortest
-shortest = sys.maxsize
-
-def find_route(st,loc,distance,start_pos):
-    global shortest
-    if st == 0 :
-        if routes[loc][start_pos]!=0 :
-            shortest = min(distance+routes[loc][start_pos],shortest)
-        return
-
-    for i in range(n):
-        if not (visited[i] or routes[loc][i]==0):
-            visited[i] = True
-            distance = distance + routes[loc][i]
-            find_route(st-1,i,distance,start_pos)
-            visited[i] = False
-            distance = distance - routes[loc][i] 
-           
+dp = [[dict() for _ in range(n)]for _ in range(n)]
 
 for i in range(n):
-    visited[i] = True
-    find_route(n-1,i,0,i)
-    visited[i] = False
+    for j in range(n):
+        minDist = dict()
+        if graph[i][j] != 0:
+            minDist[2**i|2**j] = graph[i][j]
+            dp[i][j] = minDist
+        else:
+            dp[i][j] = minDist
 
-print(shortest)
+t = 2
+while(t<n):
+    t+=1
+    ndp = [[dict() for _ in range(n)]for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            for visit in dp[i][j].keys():
+                for k in range(n):
+                    if visit & 1<<k == 0:
+                        if graph[k][i] != 0:
+                            if visit|1<<k in ndp[k][j]:
+                                ndp[k][j][visit|1<<k] = min(ndp[k][j][visit|1<<k],dp[i][j][visit]+graph[k][i])
+                            else :
+                                ndp[k][j][visit|1<<k] = dp[i][j][visit]+graph[k][i]
+                        if graph[j][k] !=0:
+                            if visit|1<<k in ndp[i][k]:
+                                ndp[i][k][visit|1<<k] = min(ndp[i][k][visit|1<<k],dp[i][j][visit]+graph[j][k])
+                            else :
+                                ndp[i][k][visit|1<<k] = dp[i][j][visit]+graph[j][k]
+    dp = ndp
+
+
+curMin = sys.maxsize
+for i in range(n):
+    for j in range(n):
+        for dis in dp[i][j].keys():
+            if graph[j][i] != 0:
+                minCan = dp[i][j][dis]+graph[j][i]
+                if curMin > minCan:
+                    curMin = minCan
+print(curMin)
+# print(ndp)
+# for i in range(n):
+#     for j in range(n):
+#         for a in dp[i][j].keys():
+#             print(i,j,bin(a),dp[i][j][a])
+# print("time :", time.time() - start)
