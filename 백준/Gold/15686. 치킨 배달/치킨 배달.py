d409b0f4b6cde1a1ece4062a19cfa17555536518
KyoJuneHwang <163268142+kyo-hwang@users.noreply.github.com>
2024-05-18 13:07:47 +0900

    [Gold V] Title: 치킨 배달, Time: 436 ms, Memory: 31732 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -7,42 +7,43 @@ citySize,storeN = map(int,sys.stdin.readline().split())
 city = [[int(c) for c in sys.stdin.readline().split()] for _ in range(citySize)]
 
 houseLoc = []
+houseCost = []
 storeLoc = []
 
 for i in range(citySize):
     for j in range(citySize):
         if city[i][j] == 1:
-            houseLoc.append([i,j,sys.maxsize])
+            houseLoc.append([i,j])
+            houseCost.append(sys.maxsize)
         elif city[i][j] == 2:
             storeLoc.append((i,j))
 
-def renewDistance(houseLoc,whichStore):
+def renewDistance(houseCost,whichStore):
     storeRow,storeCol = storeLoc[whichStore]
     for i in range(len(houseLoc)):
-        houseRow,houseCol,dis = houseLoc[i]
-        houseLoc[i][2] = min(abs(storeRow-houseRow)+abs(storeCol-houseCol),dis)
+        houseRow,houseCol = houseLoc[i]
+        houseCost[i] = min(abs(storeRow-houseRow)+abs(storeCol-houseCol),houseCost[i])
 
-
-def sumDistance(houseLoc):
+def sumDistance(houseCost):
     result  = 0
-    for i,j,dis in houseLoc:
+    for dis in houseCost:
         result+=dis
     return result
 
 totalDis = sys.maxsize
-def backtracking(houseLoc,depth,whichStore):
+def backtracking(houseCost,depth,whichStore):
     global storeN
     global totalDis
     if depth == storeN:
-        totalDis = min(sumDistance(houseLoc),totalDis)
+        totalDis = min(sumDistance(houseCost),totalDis)
         return
     if whichStore >= len(storeLoc):
         return
-    backtracking(copy.deepcopy(houseLoc),depth,whichStore+1)
-    renewDistance(houseLoc,whichStore)
-    backtracking(houseLoc,depth+1,whichStore+1)
+    backtracking(copy.deepcopy(houseCost),depth,whichStore+1)
+    renewDistance(houseCost,whichStore)
+    backtracking(houseCost,depth+1,whichStore+1)
 
-backtracking(houseLoc,0,0)
+backtracking(houseCost,0,0)
 
 print(totalDis)
     
