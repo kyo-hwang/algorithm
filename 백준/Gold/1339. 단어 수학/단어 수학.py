48aa0d7fb3ed41c98d68e7f118dba5b429daef7b
KyoJuneHwang <163268142+kyo-hwang@users.noreply.github.com>
2024-05-24 10:07:57 +0900

    [Gold IV] Title: 단어 수학, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.py
+++ 백준/Gold/1339. 단어 수학/단어 수학.py
@@ -3,44 +3,32 @@ import sys
 
 n = int(sys.stdin.readline())
 
-words = []
-maxlen = -sys.maxsize
-misteryNums = set()
-for _ in range(n):
-    word = sys.stdin.readline().rstrip()[::-1]
-    words.append(word)
+words = [sys.stdin.readline().rstrip() for _ in range(n)]
+
+chPersists = set()
+
+for word in words:
     for ch in word:
-        misteryNums.add(ch)
-    maxlen = max(maxlen,len(word))
+        chPersists.add(ch)
 
-wordCost = dict().fromkeys(misteryNums,0)
-possibleMaxNum = 9
+chValue = dict().fromkeys(chPersists,0)
 
-for i in range(maxlen-1,-1,-1):
-    # print(misteryNums)
-    for word in words:
-        if len(word)>i:
-            wordCost[word[i]] += 10**i
+for word in words:
+    for i in range(len(word)):
+        chValue[word[i]] += 10**(len(word)-1-i)
+order = sorted(chValue.items(),key= lambda x:-x[1])
 
-wordCostSort = []
-for ch in wordCost.keys():
-    wordCostSort.append((ch,wordCost[ch]))
-
-wordCostSort.sort(key=lambda x:-x[1])
-numByCh = dict().fromkeys(misteryNums,0)
-
-for ch,cost in wordCostSort:
-    numByCh[ch] = str(possibleMaxNum)
-    possibleMaxNum -=1
-
-def wordToNum(numByCh,word):
-    numString = ""
-    for ch in word:
-        numString+=numByCh[ch]
-    return int(numString)
+numBych = dict()
+value = 9
+for ch in order:
+    numBych[ch[0]] = str(value)
+    value-=1
 
 result = 0
 for word in words:
-    result += wordToNum(numByCh,word[::-1])
+    tempNum = str()
+    for ch in word:
+        tempNum+=numBych[ch]
+    result += int(tempNum)
 
-print(result) 
+print(result)
