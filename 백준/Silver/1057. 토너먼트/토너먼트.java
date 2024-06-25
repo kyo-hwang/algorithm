854e330723e2d873e54e07424e23e9abb76c0624
KyoJuneHwang <163268142+kyo-hwang@users.noreply.github.com>
2024-06-25 09:51:40 +0900

    [Silver IV] Title: 토너먼트, Time: 136 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/1057. 토너먼트/토너먼트.java
+++ 백준/Silver/1057. 토너먼트/토너먼트.java
@@ -8,24 +8,13 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         List<Integer> a = Arrays.stream(bf.readLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
-        int rN = a.get(0);
         int k = a.get(1);
         int l = a.get(2);
         int n = 0;
         while(k!=l){
-            if(k%2==1){
-                k = k/2+1;
-            }
-            else{
-                k = k/2;
-            }
-            if(l%2==1){
-                l = l/2+1;
-            }
-            else{
-                l = l/2;
-            }
             n++;
+            k= (k+1)/2;
+            l= (l+1)/2;
         }
         System.out.println(n);
     }
