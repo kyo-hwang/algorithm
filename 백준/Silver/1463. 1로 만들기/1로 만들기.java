dbe35c281f99a251d20ee63dc51a03ca025bafc7
KyoJuneHwang <163268142+kyo-hwang@users.noreply.github.com>
2024-07-30 14:16:44 +0900

    [Silver III] Title: 1로 만들기, Time: 164 ms, Memory: 18492 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+
+        dp[1] = 0;
+
+        for(int i=2;i<n+1;i++){
+            if(i%2==0){
+                dp[i] = Math.min(dp[i/2]+1,dp[i]);
+            }
+            if(i%3==0){
+                dp[i] = Math.min(dp[i/3]+1,dp[i]);
+            }
+            if(i>1){
+                dp[i] = Math.min(dp[i-1]+1,dp[i]);
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
