97192352450ba1de2951da87120e9522041f28cc
KyoJuneHwang <163268142+kyo-hwang@users.noreply.github.com>
2024-08-02 14:48:45 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 160 ms, Memory: 15304 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -2,14 +2,14 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Main{
+public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bf.readLine());
         int[] numLine = Arrays.stream(bf.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        int[] incrementDp = new int[n+1];
-        int[] decrementDp = new int[n+1];
+        int[] incrementDp = new int[n];
+        int[] decrementDp = new int[n];
 
         Arrays.fill(incrementDp,1);
         Arrays.fill(decrementDp,1);
@@ -22,7 +22,7 @@ public class Main{
             }
         }
 
-        for(int i=n-2;i>0;i--){
+        for(int i=n-2;i>-1;i--){
             for(int j=i+1;j<n;j++){
                 if(numLine[i]>numLine[j]){
                     decrementDp[i] = Math.max(decrementDp[i],decrementDp[j]+1);
@@ -32,20 +32,7 @@ public class Main{
 
         int result = 0;
         for(int i=0;i<n;i++){
-            int point = numLine[i];
-            int inc = 0;
-            int dec = 0;
-            for(int j=0;j<i;j++){
-                if(numLine[j]<point){
-                    inc = Math.max(inc,incrementDp[j]);
-                }
-            }
-            for(int j=i+1;j<n;j++){
-                if(numLine[j]<point){
-                    dec = Math.max(dec,decrementDp[j]);
-                }
-            }
-            result = Math.max(result,inc+dec+1);
+            result = Math.max(result,incrementDp[i]+decrementDp[i]-1);
         }
 
         System.out.println(result);
