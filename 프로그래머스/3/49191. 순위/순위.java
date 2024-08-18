48a04c0b83066dd0fc73a3496586c1ce0ff580c6
KyoJuneHwang <163268142+kyo-hwang@users.noreply.github.com>
2024-08-18 16:11:51 +0900

    [level 3] Title: 순위, Time: 19.32 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+class Solution {
+    public int solution(int n, int[][] results) {
+        boolean[][] dp = new boolean[n+1][n+1];
+        
+        for(int[] result : results){
+            dp[result[0]][result[1]] = true;
+        }
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(j==i){
+                    continue;
+                }
+                for(int k=1;k<n+1;k++){
+                    if(k==i){
+                        continue;
+                    }
+                    dp[j][k] = dp[j][k]||(dp[j][i]&&dp[i][k]);
+                }
+            }
+        }
+        
+        int answer=0;
+        // for(int i=0;i<n+1;i++){
+        //     System.out.println(Arrays.toString(dp[i]));
+        // }
+        
+        int sum = 0;
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(dp[i][j]) sum++;
+            }
+            for(int j=1;j<n+1;j++){
+                if(dp[j][i]) sum++;
+            }
+            // System.out.println(sum);
+            if(sum==n-1) answer++;
+            sum = 0;
+        }
+        // System.out.println(answer);
+        return answer;
+    }
+}
