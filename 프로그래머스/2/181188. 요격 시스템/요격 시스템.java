928e6e56a475ca0111a8433895656f9f4a80e650
KyoJuneHwang <163268142+kyo-hwang@users.noreply.github.com>
2024-08-19 22:30:54 +0900

    [level 2] Title: 요격 시스템, Time: 537.27 ms, Memory: 169 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -1,22 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[][] targets) {
         int answer = 0;
-        //end 기준으로 정렬
-        Arrays.sort(targets,(e1,e2)->e1[1]-e2[1]);
-        
-        int lastWeaponLoc = 0;
-        
-        for(int[] target : targets){
-            if(target[0]>=lastWeaponLoc){
-                answer++;
-                lastWeaponLoc = target[1];
-            }
-        }
-        
-        
         return answer;
     }
-    
 }
