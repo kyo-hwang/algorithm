e4c3dae6332c9d224bd6e39413584adcdc8df637
KyoJuneHwang <163268142+kyo-hwang@users.noreply.github.com>
2024-08-22 17:04:36 +0900

    [level 3] Title: 여행경로, Time: 52.32 ms, Memory: 98.6 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164#) 
 
 ### 성능 요약
 
-메모리: 103 MB, 시간: 80.44 ms
+메모리: 98.6 MB, 시간: 52.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 21일 18:37:27
+2024년 08월 22일 17:04:33
 
 ### 문제 설명
 
--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,31 +2,89 @@ import java.util.*;
 
 class Solution {
 
-    static ArrayList<String> list = new ArrayList<>();
-    static boolean useTickets[];
-
-    public String[] solution(String[][] tickets) {
-        useTickets = new boolean[tickets.length];
-
-        dfs(0, "ICN", "ICN", tickets);
-
-        Collections.sort(list);
-
-        return list.get(0).split(" ");
+    Map<String,List<Ticket>> mapDepartureToTicket;
+    int ticketNum;
+    List<String> possiblePath = new ArrayList<>();
+    
+    public class Ticket{
+        public Ticket(String[] ticketInfo){
+            departure = ticketInfo[0];
+            destination = ticketInfo[1];
+            isUsed = false;
+        }
+        
+        String departure;
+        String destination;
+        boolean isUsed;
+        
+        public String getDestination(){
+            return destination;
+        }
+        
+        public boolean isUsed(){
+            return isUsed;
+        }
+        
+        public void use(){
+            isUsed = true;
+        }
+        
+        public void republication(){
+            isUsed = false;
+        }
     }
 
-    static void dfs(int depth, String now, String path, String[][] tickets){
-        if (depth == tickets.length) {
-            list.add(path);
+    public String[] solution(String[][] tickets) {
+        
+        mapDepartureToTicket = new HashMap<>();
+        ticketNum = tickets.length;
+        
+        for(String[] ticketInfo:tickets){
+            if(!mapDepartureToTicket.containsKey(ticketInfo[0])){
+                mapDepartureToTicket.put(ticketInfo[0],new ArrayList<>());
+            }
+            mapDepartureToTicket.get(ticketInfo[0]).add(new Ticket(ticketInfo));
+        }
+        
+        dfs("ICN",0,new StringBuffer("ICN "));
+
+        Collections.sort(possiblePath);
+
+        return possiblePath.get(0).split(" "); 
+    }
+    
+    public void dfs(String departure, int depth, StringBuffer buffer){
+        if(depth==ticketNum){
+            possiblePath.add(buffer.toString());
             return;
         }
-
-        for (int i = 0; i < useTickets.length; i++) {
-            if (!useTickets[i] && now.equals(tickets[i][0])) {
-                useTickets[i] = true;
-                dfs(depth+1, tickets[i][1], path + " " +tickets[i][1], tickets);
-                useTickets[i] = false;
+        //[["ICN", "JFK"], ["ICN", "AAD"], ["JFK", "ICN"]]
+        if(!mapDepartureToTicket.containsKey(departure)){
+            return;
+        }
+        for(Ticket ticket : mapDepartureToTicket.get(departure)){
+            if(!ticket.isUsed()){
+                ticket.use();
+                buffer.append(ticket.getDestination()).append(" ");
+                dfs(ticket.getDestination(),depth+1,buffer);
+                buffer.delete(buffer.length()-4,buffer.length());
+                ticket.republication();
             }
         }
     }
+
+//     static void dfs(int depth, String now, String path, String[][] tickets){
+//         if (depth == tickets.length) {
+//             list.add(path);
+//             return;
+//         }
+
+//         for (int i = 0; i < useTickets.length; i++) {
+//             if (!useTickets[i] && now.equals(tickets[i][0])) {
+//                 useTickets[i] = true;
+//                 dfs(depth+1, tickets[i][1], path + " " +tickets[i][1], tickets);
+//                 useTickets[i] = false;
+//             }
+//         }
+//     }
 }
