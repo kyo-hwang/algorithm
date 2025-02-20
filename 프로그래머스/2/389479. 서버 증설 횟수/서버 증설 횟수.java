fddfca82da6ca1580de971e73aae12c98abdd1bd
KyoJuneHwang <163268142+kyo-hwang@users.noreply.github.com>
2025-02-20 18:34:42 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.03 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,32 +1,18 @@
-import java.util.*;
 class Solution {
     public int solution(int[] players, int m, int k) {
-        int answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparingInt(e->e));
-        for(int i=0;i<24;i++){
-            if((pq.size()+1)*m<=players[i]){
-                int peopleAdd = players[i]-(pq.size()+1)*m+1;
-                int serverAdd;
-                if(peopleAdd%m==0){
-                    serverAdd = peopleAdd/m;
-                }else{
-                    serverAdd = (peopleAdd+(m-peopleAdd%m))/m;
-                }
-                
-                for(int j=0;j<serverAdd;j++){
-                    pq.offer(i+k-1);
-                    answer++;
-                }
-            }
+        int[] server=new int[24];
+        int cnt=0;
+        for(int i=0;i<players.length;i++) {
+            if(i>0) server[i]+=server[i-1];
             
-            while(!pq.isEmpty()){
-                if(pq.peek()==i){
-                    pq.poll();
-                }else{
-                    break;
-                }
+            int need=players[i]/m-server[i];
+            if(need>0) {
+                cnt+=need;
+                server[i]+=need;
+               
+                if(server.length>i+k) server[i+k]-=need;
             }
         }
-        return answer;
+        return cnt;
     }
 }
